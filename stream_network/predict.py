d724162ea847be1454ea549855a3ffc548b50b92
Richárd Rádli <radli.richard@gmail.com>
2023-03-08 17:16:41 +0100

    predict.py has been simplified and sped up.

--- stream_network/predict.py
+++ stream_network/predict.py
@@ -1,5 +1,4 @@
 import os
-
 import pandas as pd
 import torch
 import torch.nn.functional as func
@@ -11,7 +10,7 @@ from PIL import Image
 from const import CONST
 from config import ConfigStreamNetwork
 from stream_network import StreamNetwork
-from utils.utils import find_latest_file, segment_pills
+from utils.utils import find_latest_file
 
 cfg = ConfigStreamNetwork().parse()
 
@@ -23,7 +22,12 @@ class PillRecognition:
         self.query_image_tex = None
         self.query_image_rgb = None
         self.query_image_con = None
+
         self.network_con, self.network_rgb, self.network_tex = self.load_networks()
+
+        self.network_con.eval()
+        self.network_rgb.eval()
+        self.network_tex.eval()
 
         self.preprocess_rgb = transforms.Compose([transforms.Resize((cfg.img_size, cfg.img_size)),
                                                   transforms.ToTensor()])
@@ -32,6 +36,9 @@ class PillRecognition:
                                                       transforms.Grayscale(),
                                                       transforms.ToTensor()])
 
+    # ------------------------------------------------------------------------------------------------------------------
+    # -------------------------------------------- L O A D   N E T W O R K S -------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
     @staticmethod
     def load_networks():
         """
@@ -56,138 +63,81 @@ class PillRecognition:
 
         return network_con, network_rgb, network_tex
 
-    @staticmethod
-    def euclidean_distance(x, y):
-        """
-        Calculates the Euclidean distance, between two values.
-        :param x:
-        :param y:
-        :return: Euclidian distance.
-        """
-
-        return torch.norm(x - y)
-
-    def get_query_vector(self):
-        medicine_classes = os.listdir(CONST.dir_query_rgb)
-        query_vectors = []
+    # ------------------------------------------------------------------------------------------------------------------
+    # ---------------------------------------------- G E T   V E C T O R S ---------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
+    def get_vectors(self, contour_dir, rgb_dir, texture_dir, operation):
+        medicine_classes = os.listdir(rgb_dir)
+        vectors = []
         labels = []
 
-        for med_class in tqdm(medicine_classes, desc="Process query images"):
-            query_image_paths_con = os.listdir(os.path.join(CONST.dir_query_contour, med_class))
-            query_image_paths_rgb = os.listdir(os.path.join(CONST.dir_query_rgb, med_class))
-            query_image_paths_tex = os.listdir(os.path.join(CONST.dir_query_texture, med_class))
+        for med_class in tqdm(medicine_classes, desc="Process %s images" % operation):
+            image_paths_con = os.listdir(os.path.join(contour_dir, med_class))
+            image_paths_rgb = os.listdir(os.path.join(rgb_dir, med_class))
+            image_paths_tex = os.listdir(os.path.join(texture_dir, med_class))
 
-            for idx, (con, rgb, tex) in enumerate(zip(query_image_paths_con, query_image_paths_rgb,
-                                                      query_image_paths_tex)):
+            for idx, (con, rgb, tex) in enumerate(zip(image_paths_con, image_paths_rgb, image_paths_tex)):
+                con_image = Image.open(os.path.join(contour_dir, med_class, con))
+                con_image = self.preprocess_con_tex(con_image)
 
-                con_query_image = Image.open(os.path.join(CONST.dir_query_contour, med_class, con))
-                con_query_image = self.preprocess_con_tex(con_query_image)
+                rgb_image = Image.open(os.path.join(rgb_dir, med_class, rgb))
+                rgb_image = self.preprocess_rgb(rgb_image)
 
-                rgb_query_image = Image.open(os.path.join(CONST.dir_query_rgb, med_class, rgb))
-                rgb_query_image = self.preprocess_rgb(rgb_query_image)
-
-                tex_query_image = Image.open(os.path.join(CONST.dir_query_texture, med_class, tex))
-                tex_query_image = self.preprocess_con_tex(tex_query_image)
+                tex_image = Image.open(os.path.join(texture_dir, med_class, tex))
+                tex_image = self.preprocess_con_tex(tex_image)
 
                 with torch.no_grad():
-                    query_vector1 = self.network_con(con_query_image.unsqueeze(0)).squeeze()
-                    query_vector2 = self.network_rgb(rgb_query_image.unsqueeze(0)).squeeze()
-                    query_vector3 = self.network_tex(tex_query_image.unsqueeze(0)).squeeze()
-                query_vector = torch.cat((query_vector1, query_vector2, query_vector3), dim=0)
-                query_vectors.append(query_vector)
+                    vector1 = self.network_con(con_image.unsqueeze(0)).squeeze()
+                    vector2 = self.network_rgb(rgb_image.unsqueeze(0)).squeeze()
+                    vector3 = self.network_tex(tex_image.unsqueeze(0)).squeeze()
+                vector = torch.cat((vector1, vector2, vector3), dim=0)
+                vectors.append(vector)
                 labels.append(med_class)
 
-        return query_vectors, labels
+        return vectors, labels
 
-# ----------------------------------------------------------------------------------------------------------------------
-# ----------------------------------------------- G E T   R E F   V E C ------------------------------------------------
-# ----------------------------------------------------------------------------------------------------------------------
-    def get_ref_vectors(self):
-        medicine_classes = os.listdir(CONST.dir_bounding_box)
-        reference_vectors = []
-        label = []
-
-        for med_class in tqdm(medicine_classes, desc="Process reference images"):
-            reference_image_paths_con = os.listdir(os.path.join(CONST.dir_contour, med_class))
-            reference_image_paths_rgb = os.listdir(os.path.join(CONST.dir_bounding_box, med_class))
-            reference_image_paths_tex = os.listdir(os.path.join(CONST.dir_texture, med_class))
-
-            for idx, (con, rgb, tex) in enumerate(zip(reference_image_paths_con, reference_image_paths_rgb,
-                                                      reference_image_paths_tex)):
-                con_ref_image = Image.open(os.path.join(CONST.dir_contour, med_class, con))
-                con_ref_image = self.preprocess_con_tex(con_ref_image)
-
-                rgb_ref_image = Image.open(os.path.join(CONST.dir_bounding_box, med_class, rgb))
-                rgb_ref_image = self.preprocess_rgb(rgb_ref_image)
-
-                tex_ref_image = Image.open(os.path.join(CONST.dir_texture, med_class, tex))
-                tex_ref_image = self.preprocess_con_tex(tex_ref_image)
-
-                with torch.no_grad():
-                    reference_vector1 = self.network_con(con_ref_image.unsqueeze(0)).squeeze()
-                    reference_vector2 = self.network_rgb(rgb_ref_image.unsqueeze(0)).squeeze()
-                    reference_vector3 = self.network_tex(tex_ref_image.unsqueeze(0)).squeeze()
-                reference_vector = torch.cat((reference_vector1, reference_vector2, reference_vector3), dim=0)
-                reference_vectors.append(reference_vector)
-                label.append(med_class)
-
-        return reference_vectors, label
-
+    # ------------------------------------------------------------------------------------------------------------------
+    # ------------------------------- M E A S U R E   C O S S I M   A N D   E U C D I S T ------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
     @staticmethod
-    def measure_similarity_cos_sim(q_labels, r_labels, reference_vectors, query_vectors):
-        similarity_scores_cosine = []
-        predicted_medicine = []
-        corresp_sim = []
+    def measure_similarity_and_distance(q_labels, r_labels, reference_vectors, query_vectors):
+        similarity_scores_cos_sim = []
+        similarity_scores_euc_dist = []
+        predicted_medicine_cos_sim = []
+        corresp_sim_cos_sim = []
+        predicted_medicine_euc_dist = []
+        corresp_sim_euc_dist = []
 
-        for idx_query, query_vector in tqdm(enumerate(query_vectors), total=len(query_vectors), desc="Query vectors"):
+        for idx_query, query_vector in tqdm(enumerate(query_vectors), total=len(query_vectors),
+                                            desc="Cosine similarity processing"):
             scores = []
+            scores_e = []
             for idx_ref, reference_vector in enumerate(reference_vectors):
                 score = torch.nn.functional.cosine_similarity(query_vector, reference_vector, dim=0).item()
                 scores.append(score)
 
-            similarity_scores_cosine.append(scores)
+                score_e = torch.pairwise_distance(query_vector, reference_vector).item()
+                scores_e.append(score_e)
 
-            most_similar_indices = [scores.index(max(scores)) for scores in similarity_scores_cosine]
+            similarity_scores_cos_sim.append(scores)
+            similarity_scores_euc_dist.append(scores_e)
+
+            most_similar_indices = [scores.index(max(scores)) for scores in similarity_scores_cos_sim]
             most_similar_indices_and_scores = [(i, max(scores)) for i, scores in
-                                               enumerate(similarity_scores_cosine)]
+                                               enumerate(similarity_scores_cos_sim)]
 
-            predicted_medicine.append(r_labels[most_similar_indices[idx_query]])
-            corresp_sim.append(most_similar_indices_and_scores[idx_query][1])
+            predicted_medicine_cos_sim.append(r_labels[most_similar_indices[idx_query]])
+            corresp_sim_cos_sim.append(most_similar_indices_and_scores[idx_query][1])
 
-        df = pd.DataFrame(list(zip(q_labels, predicted_medicine, corresp_sim)),
-                          columns=['GT Medicine Name', 'Predicted Medicine Name', 'Cosine similarity'])
+            most_similar_indices_e = [scores.index(min(scores)) for scores in similarity_scores_euc_dist]
+            most_similar_indices_and_scores_e = [(i, min(scores)) for i, scores in
+                                               enumerate(similarity_scores_euc_dist)]
 
-        pd.set_option('display.max_rows', None)
-        pd.set_option('display.max_columns', None)
-        pd.set_option('display.width', None)
-        pd.set_option('display.max_colwidth', None)
+            predicted_medicine_euc_dist.append(r_labels[most_similar_indices_e[idx_query]])
+            corresp_sim_euc_dist.append(most_similar_indices_and_scores_e[idx_query][1])
 
-        print(df)
-
-        return q_labels, predicted_medicine
-
-    def measure_similarity_euc_dist(self, q_labels, r_labels, reference_vectors, query_vectors):
-        similarity_scores_euclidean = []
-        predicted_medicine = []
-        corresp_sim = []
-
-        for idx_query, query_vector in tqdm(enumerate(query_vectors), total=len(query_vectors), desc="Query vectors"):
-            scores = []
-            for idx_ref, reference_vector in enumerate(reference_vectors):
-                score = self.euclidean_distance(query_vector, reference_vector)
-                scores.append(score)
-
-            similarity_scores_euclidean.append(scores)
-
-            most_similar_indices = [scores.index(min(scores)) for scores in similarity_scores_euclidean]
-            most_similar_indices_and_scores = [(i, min(scores)) for i, scores in
-                                               enumerate(similarity_scores_euclidean)]
-
-            predicted_medicine.append(r_labels[most_similar_indices[idx_query]])
-            corresp_sim.append(most_similar_indices_and_scores[idx_query][1])
-
-        df = pd.DataFrame(list(zip(q_labels, predicted_medicine, corresp_sim)),
-                          columns=['GT Medicine Name', 'Predicted Medicine Name', 'Euclidean distance'])
+        df = pd.DataFrame(list(zip(q_labels, predicted_medicine_cos_sim, predicted_medicine_euc_dist)),
+                          columns=['GT Medicine Name', 'Predicted Medicine Name (CS)', 'Predicted Medicine Name (ED)'])
 
         pd.set_option('display.max_rows', None)
         pd.set_option('display.max_columns', None)
@@ -196,14 +146,7 @@ class PillRecognition:
 
         print(df)
 
-        return q_labels, predicted_medicine
-
-
-    @staticmethod
-    def draw_results_on_image(key, value):
-        image = os.path.join(CONST.dir_test_images, '054_algoflex_s1_5_a_f4.png')
-        mask = os.path.join(CONST.dir_unet_output, '054_algoflex_s1_5_a_f4_OUT.png')
-        segment_pills(image, mask, key, value)
+        return q_labels, predicted_medicine_cos_sim, predicted_medicine_euc_dist
 
     @staticmethod
     def measure_accuracy(gt, pred):
@@ -214,16 +157,19 @@ class PillRecognition:
         print(f"Accuracy: {count / len(gt)}")
 
     def main(self):
-        query_vecs, q_labels = self.get_query_vector()
-        ref_vecs, r_labels = self.get_ref_vectors()
+        query_vecs, q_labels = self.get_vectors(contour_dir=CONST.dir_query_contour,
+                                                rgb_dir=CONST.dir_query_rgb,
+                                                texture_dir=CONST.dir_query_texture,
+                                                operation="query")
 
-        gt, pred = self.measure_similarity_cos_sim(q_labels, r_labels, ref_vecs, query_vecs)
-        self.measure_accuracy(gt, pred)
+        ref_vecs, r_labels = self.get_vectors(contour_dir=CONST.dir_contour,
+                                              rgb_dir=CONST.dir_bounding_box,
+                                              texture_dir=CONST.dir_texture,
+                                              operation="reference")
 
-        gt, pred = self.measure_similarity_euc_dist(q_labels, r_labels, ref_vecs, query_vecs)
-        self.measure_accuracy(gt, pred)
-
-        # self.draw_results_on_image(key, value)
+        gt, pred_cs, pred_ed = self.measure_similarity_and_distance(q_labels, r_labels, ref_vecs, query_vecs)
+        self.measure_accuracy(gt, pred_cs)
+        self.measure_accuracy(gt, pred_ed)
 
 
 if __name__ == "__main__":
