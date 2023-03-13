50e03e35df0112ab9a85a0ef82ec1f3bcf316013
Richárd Rádli <radli.richard@gmail.com>
2023-03-13 08:53:36 +0100

    Minor changes in triplet_loss.py

--- stream_network/triplet_loss.py
+++ stream_network/triplet_loss.py
@@ -2,47 +2,54 @@ import torch
 import torch.nn.functional as F
 
 class TripletLossWithHardMining(torch.nn.Module):
-    def __init__(self, margin=0.5):
+    def __init__(self, margin=1.0):
         super(TripletLossWithHardMining, self).__init__()
         self.margin = margin
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.hardest_positive_indices = None
+        self.hardest_negative_indices = None
 
     def forward(self, anchor_embeddings, positive_embeddings, negative_embeddings):
-
         d_ap = F.pairwise_distance(anchor_embeddings, positive_embeddings, 2)
-        d_an = self.hardest_negative(anchor_embeddings, positive_embeddings, negative_embeddings)
-        d_pp = self.hardest_positive(anchor_embeddings, positive_embeddings, negative_embeddings)
-
+        d_an, self.hardest_negative_indices = self.hardest_negative(anchor_embeddings, positive_embeddings,
+                                                                    negative_embeddings)
+        d_pp, self.hardest_positive_indices = self.hardest_positive(anchor_embeddings, positive_embeddings,
+                                                                    negative_embeddings)
         loss = torch.mean(torch.clamp(d_ap - 0.5 * d_pp - 0.5 * d_an + self.margin, min=0))
-
         return loss
 
     def hardest_negative(self, anchor_embeddings, positive_embeddings, negative_embeddings):
         d_an = []
-        for anchor_embedding, positive_embedding in zip(anchor_embeddings, positive_embeddings):
+        hardest_negative_indices = []
+        for i, (anchor_embedding, positive_embedding) in enumerate(zip(anchor_embeddings, positive_embeddings)):
             dists = F.pairwise_distance(anchor_embedding.unsqueeze(0), negative_embeddings, 2)
             dists = dists.to(self.device)
             if (dists < F.pairwise_distance(anchor_embedding.unsqueeze(0), positive_embedding.unsqueeze(0),
                                             2)).sum() == 0:
                 d_an.append(torch.tensor(0.0).to(self.device))
+                hardest_negative_indices.append(i)
             else:
-                d_an.append(torch.max(dists[dists < F.pairwise_distance(anchor_embedding.unsqueeze(0),
-                                                                        positive_embedding.unsqueeze(0), 2)]).to(
-                    self.device))
+                hard_neg_idx = torch.argmax(dists[dists < F.pairwise_distance(anchor_embedding.unsqueeze(0),
+                                                                        positive_embedding.unsqueeze(0), 2)])
+                d_an.append(dists[hard_neg_idx].to(self.device))
+                hardest_negative_indices.append(hard_neg_idx.item())
 
-        return torch.mean(torch.stack(d_an)) if len(d_an) > 0 else torch.tensor(0.0)
+        return torch.mean(torch.stack(d_an)), hardest_negative_indices
 
     def hardest_positive(self, anchor_embeddings, positive_embeddings, negative_embeddings):
         d_pp = []
-        for anchor_embedding, negative_embedding in zip(anchor_embeddings, negative_embeddings):
+        hardest_positive_indices = []
+        for i, (anchor_embedding, negative_embedding) in enumerate(zip(anchor_embeddings, negative_embeddings)):
             dists = F.pairwise_distance(anchor_embedding.unsqueeze(0), positive_embeddings, 2)
             dists = dists.to(self.device)
             if (dists > F.pairwise_distance(anchor_embedding.unsqueeze(0), negative_embedding.unsqueeze(0),
                                             2)).sum() == 0:
                 d_pp.append(torch.tensor(0.0).to(self.device))
+                hardest_positive_indices.append(i)
             else:
-                d_pp.append(torch.min(dists[dists > F.pairwise_distance(anchor_embedding.unsqueeze(0),
-                                                                        negative_embedding.unsqueeze(0), 2)]).to(
-                    self.device))
+                hard_pos_idx = torch.argmin(dists[dists > F.pairwise_distance(anchor_embedding.unsqueeze(0),
+                                                                        negative_embedding.unsqueeze(0), 2)])
+                d_pp.append(dists[hard_pos_idx].to(self.device))
+                hardest_positive_indices.append(hard_pos_idx.item())
 
-        return torch.mean(torch.stack(d_pp)) if len(d_pp) > 0 else torch.tensor(0.0)
+        return torch.mean(torch.stack(d_pp)), hardest_positive_indices
