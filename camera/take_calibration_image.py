44e53217d56a9435c499ac6d53b7def788b8c61b
Richárd Rádli <77007078+richardRadli@users.noreply.github.com>
2024-01-08 14:49:44 +0100

    Update take_calibration_image.py

--- camera/take_calibration_image.py
+++ camera/take_calibration_image.py
@@ -1,46 +1,40 @@
 import cv2
+import logging
 import os
-
 from datetime import datetime
+
+from logger_setup import setup_logger
 
 
 class CalibrationImageCapture:
     def __init__(self):
+        setup_logger()
         self.timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        self.y_max = None
-        self.x_max = None
-        self.y_min = None
-        self.x_min = None
         self.cap = None
         self.capture_count = 0
-        self.crop_size = 1600
-        self.root_dir = "C:/Users/ricsi/Desktop/cam/"
+        self.size_coeff = 3
+        self.root_dir = "C:/Projects/IVM"
         self.setup_camera()
 
     def setup_camera(self):
         self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
         if not self.cap.isOpened():
-            print("Could not open camera device")
+            logging.error("Could not open camera device")
             exit(1)
 
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 3264)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 2448)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 3480-1)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 2160-1)
         self.cap.set(cv2.CAP_PROP_AUTO_WB, 0.0)
         self.cap.set(cv2.CAP_PROP_WB_TEMPERATURE, 2500)
 
-        self.x_min = (self.cap.get(cv2.CAP_PROP_FRAME_WIDTH) - self.crop_size) // 2
-        self.y_min = (self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT) - self.crop_size) // 2
-        self.x_max = self.x_min + self.crop_size
-        self.y_max = self.y_min + self.crop_size
-
     def capture_images(self):
-        location = os.path.join(self.root_dir, "images", "camera_calibration_images", f"{self.timestamp}")
+        location = os.path.join(self.root_dir, "camera_calibration_images", f"{self.timestamp}")
         os.makedirs(location, exist_ok=True)
-        os.chdir(location)
-
+       
         while True:
-            ret, frame = self.cap.read()
-            resized_frame = frame.copy()[int(self.y_min):int(self.y_max), int(self.x_min):int(self.x_max)]
+            _, frame = self.cap.read()
+            resized_frame = cv2.resize(frame, dsize=(frame.shape[1]//self.size_coeff, 
+                                                     frame.shape[0]//self.size_coeff))
 
             cv2.imshow('Calibration Image', resized_frame)
             key = cv2.waitKey(1)
@@ -49,8 +43,9 @@ class CalibrationImageCapture:
                 break
             elif key == ord("c"):
                 filename = f"{self.capture_count:03d}.png"
-                cv2.imwrite(filename, frame)
-                print("Captured image:", filename)
+                filename_location = os.path.join(location, filename)
+                cv2.imwrite(filename_location, frame)
+                logging.info(f"Captured image: {filename}")
                 self.capture_count += 1
 
         self.cap.release()
