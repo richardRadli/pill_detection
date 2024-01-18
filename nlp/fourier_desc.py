866982134a1f625c46d59b32fa2f6c4189faaf9a
ricsi <radli.richard@gmail.com>
2024-01-18 10:11:51 +0100

    Fourier descriptor updated, still in progress.

--- nlp/fourier_desc.py
+++ nlp/fourier_desc.py
@@ -1,36 +1,182 @@
 import cv2
+import gc
+import matplotlib.pyplot as plt
 import numpy as np
 import pyefd
+import os
 
-from glob import glob
-from scipy.spatial.distance import directed_hausdorff
+from scipy.spatial.distance import directed_hausdorff, euclidean, pdist, squareform
 
 
-def elliptic_fourier(image, index):
-    contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+def plot_efd(filename, coeffs, locus=(0.0, 0.0), image=None, contour=None, n=300):
+    N = coeffs.shape[0]
+    N_half = int(np.ceil(N / 2))
+    n_rows = 2
+
+    t = np.linspace(0, 1.0, n)
+    xt = np.ones((n,)) * locus[0]
+    yt = np.ones((n,)) * locus[1]
+
+    for n in range(coeffs.shape[0]):
+        xt += (coeffs[n, 0] * np.cos(2 * (n + 1) * np.pi * t)) + (
+                coeffs[n, 1] * np.sin(2 * (n + 1) * np.pi * t)
+        )
+        yt += (coeffs[n, 2] * np.cos(2 * (n + 1) * np.pi * t)) + (
+                coeffs[n, 3] * np.sin(2 * (n + 1) * np.pi * t)
+        )
+        ax = plt.subplot2grid((n_rows, N_half), (n // N_half, n % N_half))
+        ax.set_title(str(n + 1))
+
+        if image is not None:
+            # A background image of shape [rows, cols] gets transposed by imshow so that the first dimension is vertical
+            # and the second dimension is horizontal. This implies swapping the x and y axes when plotting a curve.
+            if contour is not None:
+                ax.plot(contour[:, 1], contour[:, 0], "c--", linewidth=2)
+            ax.plot(yt, xt, "r", linewidth=2)
+            ax.imshow(image, cmap="gray")
+        else:
+            # Without a background image, no transpose is implied. This case is useful when (x,y) point clouds
+            # without relation to an image are to be handled.
+            if contour is not None:
+                ax.plot(contour[:, 0], contour[:, 1], "c--", linewidth=2)
+            ax.plot(xt, yt, "r", linewidth=2)
+            ax.axis("equal")
+
+    # plt.show()
+    plt.tight_layout()
+    plt.savefig(f"C:/Users/ricsi/Desktop/fourier/{filename}")
+    plt.close()
+    gc.collect()
+
+
+def preprocess_image(image):
+    sobel_x = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=3)
+    sobel_y = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=3)
+    edge_map = cv2.add(np.abs(sobel_x), np.abs(sobel_y))
+
+    _, thresholded_edge_map = cv2.threshold(edge_map, 10, 255, cv2.THRESH_BINARY)
+    dilated_edge_map = cv2.dilate(thresholded_edge_map, kernel=np.ones((5, 5), np.uint8), iterations=1)
+    dilated_edge_map = dilated_edge_map.astype(np.uint8)
+    _, labels, stats, centroids = cv2.connectedComponentsWithStats(dilated_edge_map)
+
+    sorted_indices = np.argsort(stats[:, 4])[::-1]
+    second_largest_index = sorted_indices[1]
+
+    pill_mask = np.zeros_like(dilated_edge_map)
+    pill_mask[labels == second_largest_index] = 255
+
+    return cv2.bitwise_and(image, image, mask=pill_mask)
+
+
+def elliptic_fourier(image, segmented_image, normalize, filename=None):
+    contours, _ = cv2.findContours(segmented_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     largest_contour = max(contours, key=cv2.contourArea)
     largest_contour = np.squeeze(largest_contour)
 
-    number_of_points = largest_contour.shape[0]
-    locus = pyefd.calculate_dc_coefficients(largest_contour)
-    coefficients = pyefd.elliptic_fourier_descriptors(largest_contour, order=5)
+    if normalize:
+        coefficients = pyefd.elliptic_fourier_descriptors(largest_contour, order=5, normalize=True)
+        np.testing.assert_almost_equal(coefficients[0, 0], 1.0, decimal=14)
+        np.testing.assert_almost_equal(coefficients[0, 1], 0.0, decimal=14)
+        np.testing.assert_almost_equal(coefficients[0, 2], 0.0, decimal=14)
+        return coefficients.flatten()[3:]
+    else:
+        number_of_points = largest_contour.shape[0]
+        locus = pyefd.calculate_dc_coefficients(largest_contour)
+        coefficients = pyefd.elliptic_fourier_descriptors(largest_contour, order=5, normalize=False)
 
-    rotated_image = cv2.transpose(image)
-    pyefd.plot_efd(index, coefficients, locus, rotated_image, largest_contour)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        rotated_image = cv2.transpose(image)
+        plot_efd(filename, coefficients, locus, rotated_image, largest_contour)
 
-    reconstruction = pyefd.reconstruct_contour(coefficients, locus, number_of_points)
-    hausdorff_distance, _, _ = directed_hausdorff(reconstruction, largest_contour)
-    print(hausdorff_distance)
+        reconstruction = pyefd.reconstruct_contour(coefficients, locus, number_of_points)
+        hausdorff_distance, _, _ = directed_hausdorff(reconstruction, largest_contour)
+        print(hausdorff_distance)
+
+
+def plot_euclidiean_distances(class_averages):
+    class_labels = list(class_averages.keys())
+    class_vectors = list(class_averages.values())
+
+    distances = squareform(pdist(class_vectors, 'euclidean'))
+
+    plt.figure(figsize=(8, 8))
+    plt.imshow(distances, cmap='viridis', vmin=0, vmax=np.max(distances))
+    plt.colorbar(label='Euclidean Distance')
+
+    plt.xticks(np.arange(len(class_labels)), class_labels, rotation=45, ha='right')
+    plt.yticks(np.arange(len(class_labels)), class_labels)
+    plt.xlabel('Class Label')
+    plt.ylabel('Class Label')
+    plt.title('Pairwise Euclidean Distances between Class Averages')
+
+    # Show the plot
+    plt.tight_layout()
+    plt.savefig("C:/Users/ricsi/Desktop/euclidean_distances.png", dpi=300)
+    plt.close()
+
+
+def calculate_distance(vector1, vector2):
+    return euclidean(vector1, vector2)
+
+
+def compare_distances(class_averages):
+    # Assuming you have a new image and have extracted its Fourier descriptor
+    new_image_path = \
+        "C:/Users/ricsi/Documents/project/storage/IVM/datasets/nih/ref/10144060615/W5VCARJ2K86QFNBMK6E1!SIZIBD7CY.JPG"
+    new_image_original = cv2.imread(new_image_path, 1)
+    new_image = cv2.cvtColor(new_image_original, cv2.COLOR_BGR2GRAY)
+    new_segmented_image = preprocess_image(new_image)
+    new_fourier_descriptor = elliptic_fourier(image=new_image_original,
+                                              segmented_image=new_segmented_image,
+                                              normalize=True)
+
+    closest_class = None
+    min_distance = float('inf')
+
+    for class_label, class_vector in class_averages.items():
+        distance = calculate_distance(new_fourier_descriptor, class_vector)
+        print(f"Distance to {class_label}: {distance}")
+
+        if distance < min_distance:
+            min_distance = distance
+            closest_class = class_label
+
+    print(f"\nThe closest class to the new image is: {closest_class}")
 
 
 def main():
-    image_path = "C:/Users/ricsi/Documents/project/storage/IVM/datasets/ogyei_v2/single/splitted/test/gt_test_masks"
-    images = sorted(glob(image_path + "/*.png"))
+    file_path = "C:/Users/ricsi/Desktop/collected_images_by_shape_nih"
+    class_averages = {}
 
-    for i, img_path in enumerate(images):
-        image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-        elliptic_fourier(image, i)
+    for root, dirs, files in os.walk(file_path):
+        for class_label in dirs:
+            class_coefficients = []  # To store norm_fourier_coeff for each image in the class
+
+            class_path = os.path.join(root, class_label)
+            for file in os.listdir(class_path):
+                image_path = os.path.join(class_path, file)
+                image_original = cv2.imread(image_path, 1)
+                try:
+                    image = cv2.cvtColor(image_original, cv2.COLOR_BGR2GRAY)
+                    segmented_image = preprocess_image(image)
+                    norm_fourier_coeff = elliptic_fourier(image=image_original,
+                                                          segmented_image=segmented_image,
+                                                          normalize=True,
+                                                          filename=file)
+                    class_coefficients.append(norm_fourier_coeff)
+                except Exception as e:
+                    print(f"{e}, Could not open{image_path}")
+
+            if class_coefficients:
+                class_average = np.mean(class_coefficients, axis=0)
+                class_averages[class_label] = class_average
+
+    # plot_euclidiean_distances(class_averages)
+    compare_distances(class_averages)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Ctrl+C pressed")
