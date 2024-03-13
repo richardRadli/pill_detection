5deb46f68e4ccc5584ddcd4bd13b2a3d4e303fd5
Richárd Rádli <77007078+richardRadli@users.noreply.github.com>
2024-03-13 09:21:44 +0100

    calculate_euc_mtx.py is now fully functional

--- margin/calculate_euc_mtx.py
+++ margin/calculate_euc_mtx.py
@@ -1,7 +1,11 @@
-import itertools
 import json
+import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pandas as pd
+import seaborn as sns
+
+from sklearn.metrics.pairwise import pairwise_distances
 
 from config.config import ConfigAugmentation
 from config.config_selector import dataset_images_path_selector
@@ -32,34 +36,94 @@ def normalize_lab_values(lab_values):
     return normalized_values
 
 
+def load_json_files(dataset_name, feature_type: str):
+    path = (
+        dataset_images_path_selector(dataset_name).get("dynamic_margin").get(feature_type)
+    )
+    json_file = find_latest_file_in_directory(path, "json")
+    json_file_name = os.path.join(path, json_file)
+
+    with open(json_file_name, "r") as file:
+        values = json.load(file)
+
+    return values
+
+
+def flatten_list(input_list):
+    concatenated_list = []
+
+    for element in input_list:
+        if isinstance(element, list):
+            concatenated_list.extend(element)
+        else:
+            concatenated_list.append(element)
+
+    return concatenated_list
+
+
+def process_vectors(lab_values, fourier_desc_values, imprint_values, score_values):
+    combined_vectors = {}
+
+    for class_id in lab_values.keys():
+        combined_vectors[class_id + "_top"] = []
+        combined_vectors[class_id + "_bottom"] = []
+
+        combined_sublist_top = (
+                lab_values[class_id]['top'] +
+                fourier_desc_values[class_id]['top'] +
+                imprint_values[class_id]['top'] +
+                score_values[class_id]['top']
+        )
+
+        combined_sublist_bottom = (
+                lab_values[class_id]['bottom'] +
+                fourier_desc_values[class_id]['bottom'] +
+                imprint_values[class_id]['bottom'] +
+                score_values[class_id]['bottom']
+        )
+
+        combined_sublist_top = flatten_list(combined_sublist_top)
+        combined_sublist_bottom = flatten_list(combined_sublist_bottom)
+
+        combined_vectors[class_id + "_top"].append(combined_sublist_top)
+        combined_vectors[class_id + "_bottom"].append(combined_sublist_bottom)
+
+    return combined_vectors
+
+
 def main():
     cfg = ConfigAugmentation().parse()
+    dataset_name = cfg.dataset_name
 
-    # LAB values
-    color_vectors_path = dataset_images_path_selector(cfg.dataset_name).get("dynamic_margin").get("colour_vectors")
-    color_json_file = find_latest_file_in_directory(color_vectors_path, "json")
-    color_json_file_name = os.path.join(color_vectors_path, color_json_file)
-
-    with open(color_json_file_name, "r") as file:
-        lab_values = json.load(file)
-
+    # L*a*b* values
+    lab_values = load_json_files(dataset_name, "colour_vectors")
     norm_lab_values = normalize_lab_values(lab_values)
 
-    # Fourier desc
-    fourier_desc_path = (
-        dataset_images_path_selector(cfg.dataset_name).get("dynamic_margin").get("Fourier_saved_mean_vectors")
+    # Fourier descriptors
+    fourier_desc_values = load_json_files(dataset_name, "Fourier_saved_mean_vectors")
+    norm_fourier_desc_values = normalize_lab_values(fourier_desc_values)
 
-    )
-    fourier_json_file = find_latest_file_in_directory(fourier_desc_path, "json")
-    fourier_json_file_name = os.path.join(fourier_json_file, color_json_file)
+    # imprint vectors
+    imprint_values = load_json_files(dataset_name, "imprint_vectors")
 
-    with open(fourier_json_file_name, "r") as file:
-        fourier_desc_values = json.load(file)
+    # score vectors
+    score_values = load_json_files(dataset_name, "score_vectors")
 
-    # asd = (norm_lab_values["0"]["bottom"])
-    # fd = fourier_desc_values["0"]["bottom"]
-    # asd = (list(itertools.chain.from_iterable(asd)))
-    # print(fd + asd)
+    combined_vectors = process_vectors(norm_lab_values, norm_fourier_desc_values, imprint_values, score_values)
+
+    labels_list = []
+    for key in combined_vectors.keys():
+        labels_list.append(key)
+
+    vectors = np.concatenate([np.array(combined_vectors[key]) for key in combined_vectors.keys()])
+    distances = pairwise_distances(vectors, metric="euclidean")
+
+    df = pd.DataFrame(distances, index=labels_list, columns=labels_list)
+
+    plt.figure(figsize=(200, 200))
+    sns.heatmap(df, annot=True, cmap="viridis", fmt=".2f", annot_kws={"size": 8}, square=True)
+
+    plt.savefig("C:/Users/ricsi/Desktop/plot.png")
 
 
 if __name__ == "__main__":
